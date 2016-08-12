c05ccdc9755f501701388f652227131d4ecd639c
Simon Murray <simon.murray@datacentred.co.uk>
2016-08-12 12:39:44 +0100

    Update CI

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,18 +1,7 @@
 require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
 
-config = {
-  'main' => {
-    'logdir' => '/var/log/puppet',
-    'vardir' => '/var/lib/puppet',
-    'ssldir' => '/var/lib/puppet/ssl',
-    'rundir' => '/var/run/puppet',
-  },
-}
-
-# Install latest puppet from puppetlabs.com
-on hosts, install_puppet
-# Explicitly configure puppet to avoid warnings
-configure_puppet(config)
+run_puppet_install_helper
 
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
@@ -20,14 +9,18 @@ RSpec.configure do |c|
   c.formatter = :documentation
 
   c.before :suite do
-    # Install this module for testing
-    puppet_module_install(:source => module_root, :module_name => 'puppet')
-    # Install common master/agent deps
-    on hosts, puppet('module', 'install', 'puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
-    on hosts, puppet('module', 'install', 'puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
-    on hosts, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-    # Install master only deps
-    on master, puppet('module', 'install', 'puppetlabs-apache'), { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module', 'install', 'puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
+    hosts.each do |host|
+      # Install this module for testing
+      copy_module_to(host, :source => module_root, :module_name => 'puppet')
+
+      # Install common master/agent deps
+      on host, puppet('module', 'install', 'puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-inifile'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+
+      # Install master only deps
+      on host, puppet('module', 'install', 'puppetlabs-apache'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'puppetlabs-concat'), { :acceptable_exit_codes => [0,1] }
+    end
   end
 end
